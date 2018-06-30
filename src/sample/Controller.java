4041a53295e76d9f8eb7bfdd7fa41b45bcc390ef
Øystein <oysteinha@gmail.com>
2018-06-30 16:49:26 +0200

    Laget lagring knapp, slette knapp og området for å se gjøremål.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,20 +1,46 @@
 package sample;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 
 public class Controller {
     @FXML
     ListView todoListView;
-    ObservableList<String> gjoremaal = FXCollections.observableArrayList();
-    ObservableList<String> data = FXCollections.observableArrayList();
+    @FXML
+    TextField taskfield;
+    @FXML
+    Label errorLabel;
 
     @FXML
     public void storeButton(ActionEvent event) {
-        todoListView.setItems("Hello World");
+        Button button = (Button) event.getSource();
+        if (taskfield.getText().equals("")) {
+            errorLabel.setTextFill(Color.web("#ff0000"));
+            errorLabel.setText("Du må skrive inn noe");
+        }
+        else {
+            todoListView.getItems().add(taskfield.getText());
+            errorLabel.setTextFill(Color.web("#41d33c"));
+            taskfield.setText("");
+            errorLabel.setText("Lagret");
+        }
+    }
+
+    @FXML
+    public void deleteButton(ActionEvent event) {
+        int selectedIdx = todoListView.getSelectionModel().getSelectedIndex();
+        if (selectedIdx != -1) {
+            todoListView.getItems().remove(selectedIdx);
+        }
+        else {
+            errorLabel.setTextFill(Color.web("#ff0000"));
+            errorLabel.setText("Listen er tom eller ingenting er valgt.");
+        }
     }
 }

d546d341df70e326410a147f5fd447dae8312b12
oystein <oysteinha@gmail.com>
2018-07-02 16:06:51 +0200

    Fikset bug med lagre knappen

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,10 +2,10 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
@@ -21,8 +21,6 @@ public class Controller {
 
     @FXML
     public void storeButton(ActionEvent event) {
-        Button button = (Button) event.getSource();
-
         if (taskfield.getText().equals("")) {
             errorLabel.setTextFill(Color.web("#ffffff"));
             errorLabel.setText("Du må skrive inn noe");
